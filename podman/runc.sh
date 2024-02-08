e6517953a38f43fc002f40d359f186cd0d97e3e2
Bradley A. Thornton <bthornto@redhat.com>
2024-02-08 13:16:39 -0800

    TZ

--- podman/runc.sh
+++ podman/runc.sh
@@ -1,5 +1,6 @@
 podman run -it \
     -v ~/direwolf.conf:/etc/direwolf.conf \
     -v ~/.config/chatfx/settings.toml:/etc/chatfx.toml \
+    --tz=local \
     --device /dev/snd --device /dev/bus/usb \
-    radio 
+    chatfx:latest
